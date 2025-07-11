12a12213b0ccf461b15801f67ec411904a9f3d2b
liangmiQwQ <mi_yyds@126.com>
2025-07-11 20:56:39 +0800

    feat: better command

--- src/main/java/fk/cheaters/commands/InvisCommand.java
+++ src/main/java/fk/cheaters/commands/InvisCommand.java
@@ -1,25 +1,37 @@
 package fk.cheaters.commands;
 
 import com.mojang.brigadier.Command;
-import com.mojang.brigadier.context.CommandContext;
 import fk.cheaters.AntiHack;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
 public class InvisCommand {
-  public static void register(){
+  public static void register() {
     CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-      dispatcher.register(CommandManager.literal("/invis")
+      dispatcher.register(CommandManager.literal("invis")
           .requires(source -> source.hasPermissionLevel(2)) // 需要OP等级2
           .executes(context -> execute(context.getSource()))
       );
     });
   }
 
-  private static int execute(ServerCommandSource source){
-    source.sendFeedback(() -> Text.literal(""), true);
+  private static int execute(ServerCommandSource source) {
+    if (source.getPlayer() != null) {
+      ServerPlayerEntity player = source.getPlayer();
+      if (AntiHack.invisiblePlayers.remove(player.getUuid())) {
+        source.sendFeedback(() -> Text.literal("恢复到正常状态"), true);
+      } else {
+        AntiHack.invisiblePlayers.add(player.getUuid());
+        source.sendFeedback(() -> Text.literal("进入隐身状态"), true);
+      }
+    } else {
+      source.sendFeedback(() -> Text.literal("服务器内部发生错误"), true);
+      return 0;
+    }
+
     return Command.SINGLE_SUCCESS;
   }
 }
